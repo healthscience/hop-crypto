a5063af88ed089c8ea51d7fc4b96514c7e456bc9
aboynejames <aboynejames@gmail.com>
2026-02-05 16:31:00 +0000

    wasm from cryptography-crate in HOP, schnorr etc.

---
+++ hop_crypto_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_sovereignkeypair_free: (a: number, b: number) => void;
+export const sovereignkeypair_get_public_key: (a: number) => [number, number];
+export const sovereignkeypair_new: () => number;
+export const sovereignkeypair_sign_intent: (a: number, b: number, c: number) => [number, number];
+export const verify_coherence: (a: number, b: number, c: number, d: number, e: number, f: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
